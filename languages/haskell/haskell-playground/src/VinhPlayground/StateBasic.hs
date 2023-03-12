cb0f4c8264a5569080650e7a732cc2f2a298b0c1
Vinh Chuc <vinh.chuc@gmail.com>
2023-03-12 18:43:08 +0000

    Added haskell code based on https://en.wikibooks.org/wiki/Haskell/Understanding_monads/State

---
+++ languages/haskell/haskell-playground/src/VinhPlayground/StateBasic.hs
@@ -0,0 +1,66 @@
+module VinhPlayground.StateBasic
+    (
+        monday,
+        tuesday,
+        luckyPair,
+        TurnstileState(..),
+        TurnstileOutput(..),
+    ) where
+
+data TurnstileState = Locked | Unlocked
+    deriving (Eq, Show)
+data TurnstileOutput = Thanks | Open | Tut
+    deriving (Eq, Show)
+
+coin, push :: TurnstileState -> (TurnstileOutput, TurnstileState)
+
+coin _ = (Thanks, Unlocked)
+
+push Locked = (Tut, Locked)
+push Unlocked = (Open, Locked)
+
+monday :: TurnstileState -> ([TurnstileOutput], TurnstileState)
+monday s0 =
+    let (a1, s1) = coin s0
+        (a2, s2) = push s1
+        (a3, s3) = push s2
+        (a4, s4) = coin s3
+        (a5, s5) = push s4
+    in ([a1, a2, a3, a4, a5], s5)
+
+regularPerson, distractedPerson, hastyPerson :: TurnstileState -> ([TurnstileOutput], TurnstileState)
+
+regularPerson s0 = 
+    let (a1, s1) = coin s0
+        (a2, s2) = push s1
+    in ([a1, a2], s2)
+
+
+distractedPerson s0 =
+    let (a1, s1) = coin s0
+    in ([a1], s1)
+
+hastyPerson s0 =
+    let (a1, s1) = push s0
+    in case a1 of 
+        Open -> ([a1], s1)
+        _ ->
+            let (a2, s2) = coin s1
+                (a3, s3) = push s2
+            in ([a1, a2, a3], s3)
+
+tuesday :: TurnstileState -> ([TurnstileOutput], TurnstileState)
+tuesday s0 =
+    let (as1, s1) = regularPerson s0
+        (as2, s2) = hastyPerson s1
+        (as3, s3) = distractedPerson s2
+        (as4, s4) = hastyPerson s3
+    in (as1 ++ as2 ++ as3 ++ as4, s4)
+
+luckyPair :: Bool -> TurnstileState -> (Bool, TurnstileState)
+luckyPair personChoice s0 =
+    let firstPerson = if personChoice then regularPerson else distractedPerson
+        (_, s1) = firstPerson s0
+        (a2, s2) = push s1
+    in
+        (a2 == Open, s2)
--- languages/haskell/haskell-playground/test/Spec.hs
+++ languages/haskell/haskell-playground/test/Spec.hs
@@ -1,19 +1,23 @@
-import Hithere.Lib2
+import VinhPlayground.StateBasic as S
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
 
 main :: IO ()
 main = hspec $ do
-  describe "Prelude.head" $ do
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Int)
+    describe "Prelude.head" $ do
+        it "returns the first element of a list" $ do
+            head [23 ..] `shouldBe` (23 :: Int)
 
-    it "returns the first element of an *arbitrary* list" $
-      property $ \x xs -> head (x:xs) == (x :: Int)
+        it "returns the first element of an *arbitrary* list" $
+            property $ \x xs -> head (x:xs) == (x :: Int)
 
-    it "throws an exception if used with an empty list" $ do
-      evaluate (head []) `shouldThrow` anyException
+        it "throws an exception if used with an empty list" $ do
+            evaluate (head []) `shouldThrow` anyException
 
-    it "does double things" $ do
-      doubleMe 2 `shouldBe` (4 :: Int)
+    describe "State monad" $ do
+        it "luckyPair" $ do
+            S.luckyPair False S.Locked `shouldBe` (True, S.Locked)
+            S.luckyPair False S.Unlocked `shouldBe` (True, S.Locked)
+            S.luckyPair True S.Locked `shouldBe` (False, S.Locked)
+            S.luckyPair True S.Unlocked `shouldBe` (False, S.Locked)
