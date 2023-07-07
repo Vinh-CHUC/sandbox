28e568b9ba2578132b0d132abda1204d16a359e8
Vinh Chuc <vinh.chuc@gmail.com>
2023-07-07 19:56:32 +0000

    Solved LCA

--- languages/python/algos/trees/lower_common_ancestor.py
+++ languages/python/algos/trees/lower_common_ancestor.py
@@ -12,9 +12,9 @@ class TreeNode:
     right: Union["TreeNode", None] = None
 
     @staticmethod
-    def from_breadthfirst(arr: List[int]) -> Union["TreeNode", None]:
+    def from_breadthfirst(arr: List[Union[int, None]]) -> "TreeNode":
         if not arr:
-            return None
+            raise AssertionError
 
         elements = deque(deepcopy(arr))
 
@@ -22,16 +22,26 @@ class TreeNode:
 
         # Contains the nodes that need their left/right to be initialised
         nodes: Deque[TreeNode] = deque()
-        root = TreeNode(elements.popleft())
+
+        val = elements.popleft()
+        if val is None:
+            raise AssertionError
+
+        root = TreeNode(val)
         nodes.append(root)
 
         while elements:
             node = nodes.popleft()
-            node.left = TreeNode(elements.popleft())
-            nodes.append(node.left)
+            el = elements.popleft()
+            if el:
+                node.left = TreeNode(el)
+                nodes.append(node.left)
+
             if elements:
-                node.right = TreeNode(elements.popleft())
-                nodes.append(node.right)
+                el = elements.popleft()
+                if el:
+                    node.right = TreeNode(el)
+                    nodes.append(node.right)
 
         return root
 
@@ -57,7 +67,6 @@ class TreeNode:
     def bf_traversal_rec(cls, level_nodes: List["TreeNode"]) -> List[int]:
         if not level_nodes:
             return []
-
         return [n.val for n in level_nodes] + cls.bf_traversal_rec(
             [
                 i
@@ -76,6 +85,9 @@ class TreeNode:
                 return [val] + left.df_traversal()
             case TreeNode(val=val, left=TreeNode() as left, right=TreeNode() as right):
                 return [val] + left.df_traversal() + right.df_traversal()
+            case _:
+                # Type checker is not good enough so I have to add this
+                raise AssertionError
 
     def df_traversal_iter(self) -> List[int]:
         ret = []
@@ -97,8 +109,71 @@ class TreeNode:
         return ",".join([str(i) for i in self.bf_traversal()])
 
 
-class Solution:
+@dataclass
+class Found:
+    lca: Union[int, None] = None
+    contains_p: bool = False
+    contains_q: bool = False
+
+
+class SolutionPure:
+    @classmethod
+    def do_lca(cls, node: Union[TreeNode, None], p: TreeNode, q: TreeNode) -> Found:
+        match node:
+            case None:
+                return Found()
+            case TreeNode(val, left, right):
+                found_left = cls.do_lca(left, p, q)
+                found_right = cls.do_lca(right, p, q)
+                if found_left.lca is not None:
+                    return found_left
+                elif found_right.lca is not None:
+                    return found_right
+                else:
+                    contains_p = (
+                        found_right.contains_p or found_left.contains_p or val == p.val
+                    )
+                    contains_q = (
+                        found_right.contains_q or found_left.contains_q or val == q.val
+                    )
+                    lca = val if contains_p and contains_q else None
+                    return Found(lca, contains_p, contains_q)
+
     def lowestCommonAncestor(
         self, root: TreeNode, p: TreeNode, q: TreeNode
     ) -> TreeNode:
-        return TreeNode(5)
+        found = self.do_lca(root, p, q)
+        if found.lca is not None:
+            return TreeNode(val=found.lca)
+        else:
+            raise RuntimeError("Not found")
+
+
+Solution = SolutionPure
+
+assert (
+    Solution()
+    .lowestCommonAncestor(
+        TreeNode.from_breadthfirst([3, 5, 1, 6, 2, 0, 8, None, None, 7, 4]),
+        TreeNode(5),
+        TreeNode(1),
+    )
+    .val
+    == 3
+)
+assert (
+    Solution()
+    .lowestCommonAncestor(
+        TreeNode.from_breadthfirst([3, 5, 1, 6, 2, 0, 8, None, None, 7, 4]),
+        TreeNode(5),
+        TreeNode(4),
+    )
+    .val
+    == 5
+)
+assert (
+    Solution()
+    .lowestCommonAncestor(TreeNode.from_breadthfirst([1, 2]), TreeNode(1), TreeNode(2))
+    .val
+    == 1
+)
