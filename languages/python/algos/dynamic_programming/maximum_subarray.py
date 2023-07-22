21916dba15e66a33ac22d5109448a5163a93d184
Vinh Chuc <vinh.chuc@gmail.com>
2023-07-22 20:15:55 +0000

    More variations to maximum_subarray

--- languages/python/algos/dynamic_programming/maximum_subarray.py
+++ languages/python/algos/dynamic_programming/maximum_subarray.py
@@ -47,3 +47,50 @@ Solution = SolutionN
 assert Solution().maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
 assert Solution().maxSubArray([1]) == 1
 assert Solution().maxSubArray([5, 4, -1, 7, 8]) == 23
+
+
+class SolutionN_B:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sub_array = -inf
+
+        DP: List[int] = []
+
+        for idx, n in enumerate(nums):
+            if idx == 0:
+                DP.append(n)
+            else:
+                if DP[idx-1] < 0:
+                    DP.append(n)
+                else:
+                    DP.append(DP[idx-1] + n)
+            max_sub_array = max(max_sub_array, DP[idx])
+
+        return max_sub_array
+
+
+Solution = SolutionN_B
+
+
+assert Solution().maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
+assert Solution().maxSubArray([1]) == 1
+assert Solution().maxSubArray([5, 4, -1, 7, 8]) == 23
+
+
+class SolutionN_C:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sub_array = -inf
+        max_sub_array_idx = -inf
+
+        for n in nums:
+            max_sub_array_idx = max(max_sub_array_idx + n, n)
+            max_sub_array = max(max_sub_array_idx, max_sub_array)
+
+        return max_sub_array
+
+
+Solution = SolutionN_C
+
+
+assert Solution().maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6
+assert Solution().maxSubArray([1]) == 1
+assert Solution().maxSubArray([5, 4, -1, 7, 8]) == 23
