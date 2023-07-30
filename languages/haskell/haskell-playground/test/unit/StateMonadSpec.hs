f9dbfdeb48be8ee9cfbbbbb385706b7f4a927ac3
Vinh Chuc <vinh.chuc@gmail.com>
2023-07-30 17:50:57 +0000

    Improve haskell tests setup

---
+++ languages/haskell/haskell-playground/test/unit/StateMonadSpec.hs
@@ -0,0 +1,13 @@
+module StateMonadSpec where
+
+import VinhPlayground.StateBasic as S
+import Test.Hspec
+
+spec :: Spec
+spec = do
+    describe "State monad" $ do
+        it "luckyPair" $ do
+            S.luckyPair False S.Locked `shouldBe` (True, S.Locked)
+            S.luckyPair False S.Unlocked `shouldBe` (True, S.Locked)
+            S.luckyPair True S.Locked `shouldBe` (False, S.Locked)
+            S.luckyPair True S.Unlocked `shouldBe` (False, S.Locked)
