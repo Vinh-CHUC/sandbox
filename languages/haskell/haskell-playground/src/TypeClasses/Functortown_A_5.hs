25afdb72a9dfe0bd2b6aba2116d6a7118d1df25f
Vinh Chuc <vinh.chuc@gmail.com>
2023-10-07 16:52:00 +0000

    Added Functortown_A_5

---
+++ languages/haskell/haskell-playground/src/TypeClasses/Functortown_A_5.hs
@@ -0,0 +1,18 @@
+module TypeClasses.Functortown_A_5 (
+) where
+
+import Data.Bifunctor
+
+data IncrementPair a b = IncrementPair a b deriving (Show, Eq)
+
+-- class Bifunctor (p :: * -> * -> *) where
+--   bimap :: (a -> b) -> (c -> d) -> p a c -> p b d
+--   first :: (a -> b) -> p a c -> p b c
+--   second :: (b -> c) -> p a b -> p a c
+--   {-# MINIMAL bimap | first, second #-}
+
+instance Bifunctor IncrementPair where
+    bimap f g (IncrementPair int r) = IncrementPair (f int) (g r)
+
+_ = first (+1) (IncrementPair 0 0)
+_ = bimap (+5) (*100) (IncrementPair 0 0)
