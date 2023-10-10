cf98e62c65328f5cefa4cb17ef506d2eb21c824d
Vinh Chuc <vinh.chuc@gmail.com>
2023-10-10 18:47:14 +0000

    Add Functortow_A_7 and tests

--- languages/haskell/haskell-playground/test/unit/FunctortownSpec.hs
+++ languages/haskell/haskell-playground/test/unit/FunctortownSpec.hs
@@ -1,7 +1,10 @@
 module FunctortownSpec where
 
+import Data.Bifunctor
 import Test.Hspec
 import Test.QuickCheck
+
+import TypeClasses.Functortown_A_7
 
 spec :: Spec
 spec = do
@@ -9,3 +12,20 @@ spec = do
         it "Functor composition" $ do
             property $ \a b ->
                 fmap ((+1).(*100)) [a :: Int, b :: Int] == ((fmap (+1)).(fmap (*100))) [a, b]
+    describe "Bifunctor Laws" $ do
+        it "Tuple identify law" $ do
+            property $ \a b ->
+                bimap id id (a :: Char, b :: Int) == id (a, b)
+        it "These bifunctor law law" $ do
+            property $ \a b ->
+                    bimap (+5) (*3) (These (a :: Int) (b :: Int)) ==
+                        (second (*3) . first (+5)) (These a b)
+                    &&
+                    bimap (+5) (*3) (These a b) ==
+                        (first (+5) . second (*3)) (These a b)
+                    &&
+                    bimap (+5) (*3) (This a) ==
+                        (first (+5) . second (*3)) (This a)
+                    &&
+                    bimap (+5) (*3) (That b) ==
+                        (first (+5) . second (*3)) (That b)
