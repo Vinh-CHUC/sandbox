9669dcf258a4a999e787c32ed44ba2015c6a99aa
Vinh Chuc <vinh.chuc@gmail.com>
2023-10-10 18:47:30 +0000

    Add Functortow_A_7 and tests

---
+++ languages/haskell/haskell-playground/src/TypeClasses/Functortown_A_7.hs
@@ -0,0 +1,17 @@
+module TypeClasses.Functortown_A_7 (
+    These (..)
+) where
+
+import Data.Bifunctor
+
+data These a b = This a | That b | These a b deriving (Eq, Show)
+
+instance Functor (These a) where
+    fmap _ (This a) = This a
+    fmap f (That b) = That $ f b
+    fmap f (These a b) = These a $ f b
+
+instance Bifunctor These where
+    bimap f _ (This a) = This $ f a
+    bimap _ g (That b) = That $ g b
+    bimap f g (These a b) = These (f a) (g b)
