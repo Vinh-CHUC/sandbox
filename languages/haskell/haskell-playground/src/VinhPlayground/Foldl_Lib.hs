a1b878a45c53e3f5e5aab92bebbfde8ac9d471e5
Vinh Chuc <vinh.chuc@gmail.com>
2023-12-19 19:27:08 +0000

    Modifications to Foldl_Lib

--- languages/haskell/haskell-playground/src/VinhPlayground/Foldl_Lib.hs
+++ languages/haskell/haskell-playground/src/VinhPlayground/Foldl_Lib.hs
@@ -2,8 +2,10 @@ module VinhPlayground.Foldl_Lib where
 -- https://github.com/Gabriella439/foldl
 
 import qualified Control.Foldl as L
+import Control.Applicative (Applicative (..))
+import GHC.Generics (Datatype(datatypeName))
 
--- This library is organised around this main datatype
+-- This library is organised around this main datatype Fold a b =
 -- Fold (x -> a -> x) x (x -> b)
 -- * a: type of iterable
 -- * b: type of final result
@@ -23,3 +25,18 @@ import qualified Control.Foldl as L
 
 -- The functor/applicative instance really focus on the extractor function
 -- e.g. fmapping composes on top of (x -> b) so it's like instance Functor (Fold a)
+
+sumAndLength = L.fold ((,) <$> L.sum <*> L.length)
+sumAndProductAndLength = L.fold ((,,) <$> L.sum <*> L.product <*> L.length)
+-- liftA2 style:
+sumAndLengthA2 = L.fold (liftA2 (,) L.sum L.length)
+
+-- One can also meditate on this datatypeName
+-- data FoldM m a b = FoldM (x -> a -> m x) (m x) (x -> m b)
+--                           acc             init  extractor
+--
+-- x <- init
+-- x <- acc x a1
+-- x <- acc x a2
+-- ...
+-- extractor x
