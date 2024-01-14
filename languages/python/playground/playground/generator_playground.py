1bfe44d2a5078b9b1764c608d8e72c3837160408
Vinh Chuc <vinh.chuc@gmail.com>
2024-01-14 13:05:31 +0000

    Added generator and vectorization playgrounds

---
+++ languages/python/playground/playground/generator_playground.py
@@ -0,0 +1,76 @@
+####################
+## Sending values ##
+####################
+
+from types import prepare_class
+
+
+def my_generator():
+    for i in range(10):
+        print(f"from generator {yield i}")
+
+gen = my_generator()
+
+# Will return 0 BUT generator will not print yet
+# This is because after this line executes, the yield has sent the value to the caller of send()
+# but the print() hasn't been called yet
+
+# returns the first value passed into yield inside generator
+# generator is now blocked waiting to evaluate the yield#0
+gen.send(None)  # => 0
+# the yield expression inside generator evaluates to 1, generator is not "stuck just after yield 2"
+# waiting for the next value to be sent
+gen.send(1)  # => 1
+
+
+# gen.send(..)                           yield#1 yields
+# gen.send(x1) yield#1 evaluates to x1 | yield#2 yields
+# gen.send(x2) yield#2 evaluates to x2 | yield#3 yields
+# gen.send(x3) yield#3 evaluates to x3 | yield#4 yields
+
+# An odd design choice... I guess it's because otherwise the generator would have to "hold onto"
+# the sent value until the next send().
+# - The intended semantics is such that when one sends(something) the
+# something has to be immediately passed in the generator to become the result of the yield
+# expression, so it has to be waiting already.
+# - Another way to phrase it is first the generator yields, then it receives the sent value!
+#
+# Another approach would have been for the first send to do another loop "upfront" and block just
+# before the second call to yield?
+# BUT! This would not work with things that only yield once, as we need to preseve this concept
+# of stopiteration
+
+def my_generator_2():
+    yield 42
+
+############
+## islice ##
+############
+
+def my_generator_3():
+    while True:
+        print("hi there")
+        yield "hello"
+
+## Accumulate
+def accumul_rec(arr):
+    match arr:
+        case []:
+            return 0
+        case [x]:
+            return [x]
+        case [*rest, last]:
+            temp = accumul(rest)
+            return temp + [temp[-1] + last]
+
+def accumul_it(arr):
+    if not arr:
+        return 0
+    ret = []
+
+    for idx, el in enumerate(arr):
+        if idx == 0:
+            ret.append(el)
+        else:
+            ret.append(el + ret[-1])
+    return ret
---
+++ languages/python/playground/playground/vectorization.py
@@ -0,0 +1,60 @@
+from enum import Enum
+from typing import assert_never, Any, Union, List
+
+import numpy as np
+import jax
+import jax.numpy as jnp
+from numpy.core.multiarray import where
+
+L = 10**6
+
+def some_fn(a: float, b: float):
+    if a > b:
+        return a - b
+    else:
+        return a + b
+
+class Variant(Enum):
+    BASELINE = 1
+    NUMPY = 2
+    JAX = 3
+
+def baseline(variant: Variant):
+    match variant:
+        case Variant.BASELINE:
+            return [10.0 * 1 for _ in range(L)]
+        case Variant.NUMPY:
+            return 10.0 * np.ones(L)
+        case Variant.JAX:
+            return 10.0 * jnp.ones(L)
+
+# GOTCHA!
+# The vectorize function is provided for convenience, not for performance. The implementation is
+# essentially a for loop
+NPFUNC = np.vectorize(some_fn)
+JAXFUNC = jax.vmap(some_fn, in_axes=[0, None])
+
+def vectorize(input: Union[np.ndarray, jnp.ndarray, List[Any]]) -> object:
+    match input:
+        case list():
+            return [some_fn(x, L//2) for x in input]
+        case np.ndarray():
+            return np.where(input > L//2, input - L//2, input + L//2)
+        case jnp.ndarray():
+            return jnp.where(input > L//2, input - L//2, input + L//2)
+        case _:
+            assert_never(input)
+
+"""
+base = baseline(Variant.BASELINE)
+nparr = baseline(Variant.NUMPY)
+jnparr = baseline(Variant.JAX)
+
+%timeit baseline(Variant.BASELINE)
+%timeit baseline(Variant.NUMPY)
+%timeit baseline(Variant.JAX)
+
+%timeit vectorize(base)
+%timeit vectorize(nparr)
+%timeit vectorize(jnparr)
+"""
