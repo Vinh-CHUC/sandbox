721d288c0c819d3b457c4ea17828d5197d1e9cab
Vinh Chuc <vinh.chuc@skyral.io>
2024-05-28 11:05:19 +0100

    Pydantic playground

---
+++ languages/python/playground/playground/pydantic_playground.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+class Response[T](BaseModel):
+    data: T
+
+def test():
+    r = Response[str](data=5)
+    r.model_dump_json()
+    s = Response(data=5)
+    s.model_dump_json()
