4ff0dc19506eeaf9fb0b5b0ba06b7e3d40d00803
Vinh Chuc <vinh.chuc@gmail.com>
2024-05-31 10:38:18 +0100

    Added substitute to eval.py

--- languages/python/tapl/tapl/untyped_lambda/eval.py
+++ languages/python/tapl/tapl/untyped_lambda/eval.py
@@ -6,18 +6,39 @@ from tapl.untyped_lambda.parser import Abstraction, Application, Term, Variable
 def eval(Term) -> Abstraction:
     pass
 
+
 def shift(by: int, above: int, t: Term) -> Term:
     match t:
         case Variable(index=index) if index >= above:
-            return Variable(index=index+by)
+            return Variable(index=index + by)
         case Variable(index=index):
             return t
         case Abstraction(term=term):
-            return Abstraction(term=shift(by=by, above=above+1, t=term))
+            return Abstraction(term=shift(by=by, above=above + 1, t=term))
         case Application(abstraction=a, operand=b):
-            return Application(abstraction=shift(by,above,a), operand=shift(by,above,b))
+            return Application(
+                abstraction=shift(by, above, a), operand=shift(by, above, b)
+            )
         case _:
             assert_never(t)
 
-def substitute(index: int, t: Term) -> Term:
-    pass
+
+def substitute(index: int, from_term: Term, to_term: Term) -> Term:
+    match from_term:
+        case Variable(index=idx) if idx == index:
+            return to_term
+        case Variable():
+            return from_term
+        case Abstraction(term=term):
+            return Abstraction(
+                term=substitute(
+                    index + 1, from_term=term, to_term=shift(by=1, above=0, t=to_term)
+                )
+            )
+        case Application(abstraction=a, operand=b):
+            return Application(
+                abstraction=substitute(index, a, to_term),
+                operand=substitute(index, b, to_term),
+            )
+        case _:
+            assert_never(from_term)
