7d27e80fae2b9cdefe945525bc9a757a6fba3c91
Vinh Chuc <vinh.chuc@skyral.io>
2024-09-29 22:24:35 +0100

    Applicative and Monad for parser

--- languages/haskell/haskell-playground/src/VinhPlayground/ToyParser.hs
+++ languages/haskell/haskell-playground/src/VinhPlayground/ToyParser.hs
@@ -28,6 +28,29 @@ instance Functor (Parser i e) where
     case runp input of
       Left errs -> Left errs
       Right (a, rest) -> Right (f a, rest)
+
+instance Applicative (Parser i e) where
+  pure a = Parser $ \input -> Right (a, input)
+  Parser p <*> Parser q = Parser $ \input ->
+    case p input of
+      Left errs -> Left errs
+      Right (f, rest) ->
+        case q rest of
+          Left errs -> Left errs
+          Right (a, rest') ->
+            Right (f a, rest')
+
+instance Monad (Parser i e) where
+  return = pure
+
+  Parser p >>= k = Parser $ \input ->
+    case p input of
+      Left err -> Left err
+      Right (output, rest) ->
+        let
+          Parser p' = k output
+        in
+          p' rest
   
 satisfy :: (i -> Bool) -> Parser i e i
 satisfy predicate = Parser $ \case
