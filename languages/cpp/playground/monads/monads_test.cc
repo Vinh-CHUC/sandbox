0324af9123b4d502a5c5a000e6edc3db420de149
Vinh Chuc <vinh.chuc@gmail.com>
2024-11-28 19:36:04 +0000

    Some basic examples for tl::expected

--- languages/cpp/playground/monads/monads_test.cc
+++ languages/cpp/playground/monads/monads_test.cc
@@ -2,8 +2,25 @@
 
 #include <gtest/gtest.h>
 
-
 TEST(Monads, Expected){
-  auto exp = build_expected();
-  ASSERT_TRUE(exp.has_value());
+  auto foo = tl::expected<std::string, int>{ std::string{}};
+  auto bar = foo.and_then(
+      [](const std::string& str){return tl::expected<std::string, int>{str};}
+  );
+  ASSERT_TRUE(bar.has_value());
+}
+
+TEST(Monads, Unexpected){
+  tl::expected<std::string, int> foo = tl::unexpected<int>{ 1 };
+  ASSERT_FALSE(foo.has_value());
+}
+
+TEST(Monads, AndThen){
+  auto foo = tl::expected<std::string, int>{ std::string{}};
+  auto bar = foo.and_then(
+      [](const std::string& str){
+        return tl::expected<std::string, int>{tl::unexpected<int>(1)};
+      }
+  );
+  ASSERT_FALSE(bar.has_value());
 }
