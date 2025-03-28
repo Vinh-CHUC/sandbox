007780935d16b6d4d17e52f187d05958089640e4
Vinh Chuc <vinh.chuc@skyral.io>
2025-03-28 21:42:44 +0000

    Functor concepts!!!

--- languages/cpp/playground/functor/functor.h
+++ languages/cpp/playground/functor/functor.h
@@ -4,28 +4,38 @@
 #include <functional>
 #include <memory>
 
-template <class T> using Identity = T;
-template <class T> using Ref = std::reference_wrapper<T>;
+template <class T> struct Identity {
+  T val;
+  Identity(T t): val(t){};
+  T& get(){return val;}
+};
+
+template <class T> struct Ref {
+  std::reference_wrapper<T> val;
+  Ref(T t): val(t){};
+  T& get(){return val;}
+};
+
+template <template <typename...> class F, typename T>
+concept ToRef = requires (F<T> r) {
+    { r.get() } -> std::same_as<T&>;
+};
 
 struct A {};
 struct B {};
 struct C {};
 
-template <template <typename...> class F>
+template <template <typename...> class F> requires ToRef<F, A>
 struct MyClass {
   F<A> a;
   F<B> b;
   F<C> c;
 
-  MyClass(F<A> a, F<B> b, F<C> c);
+  MyClass(F<A> _a, F<B> _b, F<C> _c): a(_a), b(_b), c(_c) {}
 };
 
 using MyClassPlain = MyClass<Identity>;
 using MyClassRef = MyClass<Ref>;
-
-template<> MyClass<Identity>::MyClass(A _a, B _b, C _c): a(_a), b(_b), c(_c) {}
-
-template<> MyClass<Ref>::MyClass(Ref<A> _a, Ref<B> _b, Ref<C> _c): a(_a), b(_b), c(_c) {}
 
 template <class T> class Functor {
 public:
