d6ae2f1069f48976289a98d36ff3e8a40578f9d5
Vinh Chuc <vinh.chuc@gmail.com>
2025-04-19 23:33:23 +0100

    Nearly got my compile error tester working

--- languages/cpp/playground/utils/build_error_tester.h
+++ languages/cpp/playground/utils/build_error_tester.h
@@ -12,21 +12,28 @@ struct ConstExprTester {
 };
 
 template<template <typename ...> class F, typename T>
-struct ConstExprTester<F, T, std::void_t<std::integral_constant<T,  std::integral_constant<int, 3>()     >>> {
-    static constexpr bool is_valid = true;
+struct ConstExprTester<
+  F, T,
+  std::void_t<
+    std::integral_constant<T, F<T>::call()>
+  >
+> {
+  static const bool is_valid = true;
 };
 
 template<typename T>
-struct A {
-  constexpr T operator()() const noexcept{
+struct A{
+  constexpr static T call() {
       T t1{}, t2{};
       return t1 * t2;
   };
 };
 
+constexpr int NINE = A<int>::call();
+static_assert(NINE == 0);
 
 static_assert(ConstExprTester<A, int>::is_valid);
-/* static_assert(!ConstExprTester<std::string, foo<std::string>>::is_valid); */
+/* static_assert(!ConstExprTester<A, std::string>::is_valid); */
 
 }
 
