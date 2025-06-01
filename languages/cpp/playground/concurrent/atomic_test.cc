f7ecb5ad45a55e1abd21ca63d7750dc411bdbe51
Vinh Chuc <vinh.chuc@gmail.com>
2025-06-01 10:54:20 +0100

    Add more content around std::atomic

--- languages/cpp/playground/concurrent/atomic_test.cc
+++ languages/cpp/playground/concurrent/atomic_test.cc
@@ -1,9 +1,59 @@
 #include <atomic>
+#include <chrono>
+#include <thread>
 
 #include <gtest/gtest.h>
+
+using namespace std::chrono_literals;
 
 TEST(Atomic, Basic){
   std::atomic<int> aint{0};
   aint++;
   // aint *= 2;
+}
+
+TEST(Threads, Basic){
+  std::vector<std::thread> threads{};
+
+  std::atomic<int> aint{0};
+
+  for(int i = 0; i < 100; i++){
+    threads.push_back(
+      std::thread([&aint](){
+        for(int i = 0; i < 100; i++){
+          aint++;
+        }
+      })
+    );
+  }
+
+  for(auto& t: threads){
+    t.join();
+  };
+
+  ASSERT_EQ(aint.load(), 10000);
+}
+
+TEST(Threads, RaceCondition){
+  std::vector<std::thread> threads{};
+
+  int hello{0};
+
+  for(int i = 0; i < 100; i++){
+    threads.push_back(
+      std::thread([&hello](){
+        for(int i = 0; i < 100; i++){
+          auto copy = hello;
+          std::this_thread::sleep_for(1ms);
+          hello = copy + 1;
+        }
+      })
+    );
+  }
+
+  for(auto& t: threads){
+    t.join();
+  };
+
+  ASSERT_EQ(hello, 10000);
 }
