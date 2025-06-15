8b39d69375b18df7ff0e16832d7d81b166927020
Vinh Chuc <vinh.chuc@skyral.io>
2025-06-15 16:11:45 +0100

    AoC #6 #1 done

--- languages/python/playground/advent_of_code/_6_guard_gallivant.py
+++ languages/python/playground/advent_of_code/_6_guard_gallivant.py
@@ -31,8 +31,66 @@ def rotate(d: Direction) -> Direction:
         case _:
             assert_never(d)
 
+def next_position(pos: Position, d: Direction) -> Position:
+    match d:
+        case Direction.NORTH:
+            return Position(pos.x - 1, pos.y)
+        case Direction.EAST:
+            return Position(pos.x, pos.y + 1)
+        case Direction.SOUTH:
+            return Position(pos.x + 1, pos.y)
+        case Direction.WEST:
+            return Position(pos.x, pos.y - 1)
+        case _:
+            assert_never(d)
+
+
 def get_data() -> list[list[str]]:
     lines = (Path(__file__).parent / "_6_guard_gallivant.dat").open().readlines()
     return [
         list(l.strip()) for l in lines
     ]
+
+
+def initial_position(data: list[list[str]]) -> Position:
+    for row_idx in range(len(data)):
+        for col_idx in range(len(data[row_idx])):
+            if data[row_idx][col_idx] == "^":
+                return Position(row_idx, col_idx)
+
+    raise RuntimeError
+
+def oob(p: Position, data: list[list[str]]) -> bool:
+    if p.x < 0 or p.x >= len(data):
+        return True
+    if p.y < 0 or p.y >= len(data[0]):
+        return True
+    return False
+
+
+def part1():
+    data = get_data()
+
+    direction = Direction.NORTH
+    position = initial_position(data)
+    data[position.x][position.y] = VISITED
+
+    while True:
+        next_p = next_position(position, direction)
+
+        if oob(next_p, data):
+            break
+        elif data[next_p.x][next_p.y] == GUARD:
+            direction = rotate(direction)
+            continue
+        else:
+            position = next_p
+            data[next_p.x][next_p.y] = VISITED
+
+    visited_count = 0
+    for row_idx in range(len(data)):
+        for col_idx in range(len(data[row_idx])):
+            if data[row_idx][col_idx] == VISITED:
+                visited_count += 1
+
+    return visited_count
---
+++ languages/python/playground/advent_of_code/test_6.py
@@ -0,0 +1,5 @@
+from advent_of_code._6_guard_gallivant import part1
+
+
+def test_part1():
+    assert part1() == 4515
