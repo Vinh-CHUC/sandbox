cf5fec453102e65c7d42ef4e1d6d919c0f1ce959
Vinh Chuc <vinh.chuc@skyral.io>
2025-06-17 15:02:03 +0100

    AoC #7

---
+++ languages/python/playground/advent_of_code/test_7.py
@@ -0,0 +1,10 @@
+from advent_of_code._7_bridge_repair import part1, part2
+
+
+def test_part1():
+    assert part1() == 663613490587
+
+
+# A bit slow
+# def test_part2():
+#     assert part2() == 110365987435001
