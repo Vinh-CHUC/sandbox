2673dc60673013f36c88a43bc30c7eaa041c406e
Vinh Chuc <vinh.chuc@skyral.io>
2025-07-05 18:23:48 +0100

    Binary vector mixtures

--- languages/python/playground/maths/proba_stats/mixture_models.py
+++ languages/python/playground/maths/proba_stats/mixture_models.py
@@ -18,3 +18,30 @@ def binary_vector(dim_or_dist: int | Dist, size=5000, rng: Generator = RNG):
 
     samples = rng.random(size=(size, dim))
     return (samples < bernoulli_params.reshape(1, -1)).astype(np.int_)
+
+
+def binary_vector_mixtures(
+    dim_or_dist: tuple[int, int] | Dist,
+    mixture_weights: Dist,
+    size=5000,
+    rng: Generator = RNG,
+):
+    match dim_or_dist:
+        case np.ndarray() as dist if (
+            len(dist.shape) == 2 and dist.shape[0] == mixture_weights.shape[0]
+        ):
+            bernoulli_mixture_params = dist
+            dims = dist.shape
+        case (int(), int()) as dims if dims[0] == mixture_weights.shape[0]:
+            bernoulli_mixture_params = rng.random(size=dims)
+        case _:
+            raise AssertionError
+
+    # 1-Dimensional simply the samples of "cluster id"
+    cluster_selections = rng.choice(
+        mixture_weights.shape[0], size=size, p=mixture_weights
+    )
+
+    # Interestingly here we draw only one sample, the sampling is done just before really
+    samples = rng.binomial(n=1, p=bernoulli_mixture_params[cluster_selections])
+    return samples
--- languages/python/playground/maths/proba_stats/test_mixture_models.py
+++ languages/python/playground/maths/proba_stats/test_mixture_models.py
@@ -1,8 +1,9 @@
 import numpy as np
+import pandas as pd
 from hypothesis import given, settings, strategies as st
-from numpy._core.fromnumeric import diagonal
 
 from maths.proba_stats import mixture_models
+from maths.proba_stats.utils import Dist
 
 
 @given(
@@ -27,3 +28,53 @@ def test_binary_vectors(seed, dim, size):
 
     # The covariances are 0
     assert np.isclose(cov - (cov.diagonal() * np.eye(cov.shape[0])), 0, atol=0.03).all()
+
+
+class TestBinaryVectorMixtures:
+    @given(
+        seed=st.integers(min_value=42),
+        size=st.integers(min_value=5000, max_value=100_000),
+    )
+    @settings(max_examples=10)
+    def test_binary_mixtures(self, seed, size):
+        """
+        A classic example, each component has 0 covariance
+        BUT when mixing this is not the case anymore
+        """
+        dist = np.array([[0.1, 0.9], [0.9, 0.1]])
+        mixture_weights = np.array([0.5, 0.5])
+
+        samples = mixture_models.binary_vector_mixtures(
+            Dist(dist),
+            Dist(mixture_weights),
+            rng=np.random.default_rng(seed=seed),
+            size=size,
+        )
+
+        df = pd.DataFrame(samples, columns=["a", "b"])
+        # The essence of it, given an observation we got more information about which cluster
+        # we're likely in. Which in turns gives information about the other one
+        assert (df[df.b == 0].a.mean() - df[df.b == 1].a.mean()) > 0.5
+
+    @given(
+        seed=st.integers(min_value=42),
+        dim=st.integers(min_value=2, max_value=50),
+        n_mixtures=st.integers(min_value=2, max_value=10),
+        size=st.integers(min_value=5000, max_value=100_000),
+    )
+    @settings(max_examples=10)
+    def test_binary_mixtures_2(self, seed, dim, n_mixtures, size):
+        """
+        A classic example, each component has 0 covariance
+        BUT when mixing this is not the case anymore
+        """
+        RNG = np.random.default_rng(seed=seed)
+        dist = RNG.random((n_mixtures, dim))
+        mixture_weights = np.array([0.5, 0.5])
+
+        samples = mixture_models.binary_vector_mixtures(
+            Dist(dist),
+            Dist(mixture_weights),
+            rng=np.random.default_rng(seed=seed),
+            size=size,
+        )
