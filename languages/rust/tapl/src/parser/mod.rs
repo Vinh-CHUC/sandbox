6358f7653bf5ece9b72a8eec1c120b81b7ca2e39
Vinh Chuc <vinh.chuc@gmail.com>
2025-08-10 18:59:59 +0100

    BKP Tapl parsers

--- languages/rust/tapl/src/parser/mod.rs
+++ languages/rust/tapl/src/parser/mod.rs
@@ -1,5 +1,30 @@
 use chumsky::prelude::*;
 
-fn parser<'src>() -> impl Parser<'src, &'src str, ()> {
+pub fn parser<'src>() -> impl Parser<'src, &'src str, ()> {
     end()
+}
+
+pub fn another_parser<'src>() -> impl Parser<'src, &'src str, ()> {
+    just("vinh").then(just("chuc")).map(|_| ())
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parser() {
+        // Our parser expects empty strings, so this should parse successfully
+        assert_eq!(parser().parse("").into_result(), Ok(()));
+
+        // Anything other than an empty string should produce an error
+        assert!(parser().parse("123").has_errors());
+    }
+
+    #[test]
+    fn test_another_parser() {
+        // Our parser expects empty strings, so this should parse successfully
+        assert_eq!(another_parser().parse("vinhchuc").into_result(), Ok(()));
+        assert!(another_parser().parse("123").has_errors());
+        assert!(another_parser().parse("vinh").has_errors());
+    }
 }
