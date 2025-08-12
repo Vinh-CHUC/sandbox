bdedc228ab426e2a1ed8b2a89de0b497a1726d58
Vinh Chuc <vinh.chuc@gmail.com>
2025-08-12 22:32:30 +0100

    More parsing

--- languages/rust/tapl/src/parser/mod.rs
+++ languages/rust/tapl/src/parser/mod.rs
@@ -8,6 +8,13 @@ pub fn another_parser<'src>() -> impl Parser<'src, &'src str, ()> {
     just("vinh").then(just("chuc")).map(|_| ())
 }
 
+pub fn flat_arithmetic_expression<'src>() -> impl Parser<'src, &'src str, u32> {
+    let int = text::int(10)
+        .from_str()
+        .unwrapped();
+    int.clone().foldl(just('+').ignore_then(int).repeated(), |a, b| a + b)
+}
+
 mod tests {
     use super::*;
 
@@ -26,5 +33,14 @@ mod tests {
         assert_eq!(another_parser().parse("vinhchuc").into_result(), Ok(()));
         assert!(another_parser().parse("123").has_errors());
         assert!(another_parser().parse("vinh").has_errors());
+    }
+
+    #[test]
+    fn test_flat_arithmetic_expresssions() {
+        assert_eq!(flat_arithmetic_expression().parse("10").into_result(), Ok(10));
+        assert_eq!(flat_arithmetic_expression().parse("10+10").into_result(), Ok(20));
+
+        // Does not cope with white spaces
+        assert!(flat_arithmetic_expression().parse("10+ 10").has_errors());
     }
 }
