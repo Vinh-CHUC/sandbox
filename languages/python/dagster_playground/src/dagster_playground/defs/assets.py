c6c3a19f4038950ea9b0ad8bec3b5fc0ec57d51f
Vinh Chuc <vinh.chuc@skyral.io>
2025-08-17 16:57:32 +0100

    Refactor IOManager, populate dynamic partitions

--- languages/python/dagster_playground/src/dagster_playground/defs/assets.py
+++ languages/python/dagster_playground/src/dagster_playground/defs/assets.py
@@ -9,14 +9,23 @@ from dagster_playground.defs.config import DataGenConfig
 Only one configuration object per asset that has to be named config
 """
 
+RNG = np.random.default_rng(seed=0)
+id_partitions = dg.DynamicPartitionsDefinition(name="id_partitions")
+
 @dg.asset(io_manager_key="parquet_io_manager")
-def processed_data(config: DataGenConfig):
+def processed_data(
+    context: dg.AssetExecutionContext,
+    config: DataGenConfig
+):
     df = pd.DataFrame(
         {
+            "some_id": RNG.choice(100, size=config.count),
             "dummy_int": np.arange(config.count),
             "dummy_str": [f"Hello {x}" for x in np.arange(config.count)],
         }
     )
+
+    context.instance.add_dynamic_partitions("id_partitions", [str(i) for i in range(10)])
 
     return df
 
--- languages/python/dagster_playground/src/dagster_playground/defs/resources.py
+++ languages/python/dagster_playground/src/dagster_playground/defs/resources.py
@@ -9,33 +9,34 @@ def mkdir(p: Path):
     p.mkdir(parents=True, exist_ok=True)
 
 
+def _get_path(
+    context: dg.OutputContext | dg.InputContext,
+    base_path: str,
+    file_ext: str
+) -> Path:
+    mkdir(Path(base_path))
+    p = Path(base_path) / Path(*context.asset_key.path)
+    partition_suffix = context.has_partition_key and ("_" + (context.partition_key or ""))
+    return Path(f"{p}_{partition_suffix}.{file_ext}")
+
+
 class PandasCSVIOManager(dg.ConfigurableIOManager):
     base_path: str = ""
 
-    def _get_path(self, context) -> Path:
-        mkdir(Path(self.base_path))
-        p = Path(self.base_path) / Path(*context.asset_key.path)
-        return Path(f"{p}.csv")
-
     def handle_output(self, context: dg.OutputContext, obj: pd.DataFrame):
-        obj.to_csv(self._get_path(context), index=False)
+        obj.to_csv(_get_path(context, self.base_path, "csv"), index=False)
 
     def load_input(self, context: dg.InputContext):
-        return pd.read_csv(self._get_path(context))
+        return pd.read_csv(_get_path(context, self.base_path, "csv"))
 
 class PandasParquetIOManager(dg.ConfigurableIOManager):
     base_path: str = ""
 
-    def _get_path(self, context) -> Path:
-        mkdir(Path(self.base_path))
-        p = Path(self.base_path) / Path(*context.asset_key.path)
-        return Path(f"{p}.parquet")
-
     def handle_output(self, context: dg.OutputContext, obj: pd.DataFrame):
-        obj.to_parquet(self._get_path(context), index=False)
+        obj.to_parquet(_get_path(context, self.base_path, "parquet"), index=False)
 
     def load_input(self, context: dg.InputContext):
-        return pd.read_parquet(self._get_path(context))
+        return pd.read_parquet(_get_path(context, self.base_path, "parquet"))
 
 DAGSTER_DEFAULT_OUTPUT_FOLDER = Path(__file__).parent.parent.parent.parent / "assets_output"
 
