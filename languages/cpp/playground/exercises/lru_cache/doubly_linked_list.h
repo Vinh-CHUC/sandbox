c9a5185c80bb9bc522af7fed46baa2db2e97102b
Vinh Chuc <vinh.chuc@gmail.com>
2025-09-21 22:20:18 +0100

    Doubly linked list in CPP

--- languages/cpp/playground/exercises/lru_cache/doubly_linked_list.h
+++ languages/cpp/playground/exercises/lru_cache/doubly_linked_list.h
@@ -1,34 +1,59 @@
-#include <functional>
 #include <generator>
 #include <memory>
 
-
-template <typename T>
-struct DLLEl {
+template <typename T> struct DLLEl {
   T val;
   std::weak_ptr<DLLEl<T>> prev;
   std::shared_ptr<DLLEl<T>> next;
 };
 
-template <typename T>
-struct DoublyLinkedList {
+template <typename T> struct DoublyLinkedList {
   std::shared_ptr<DLLEl<T>> head;
   std::shared_ptr<DLLEl<T>> tail;
 
-  DoublyLinkedList(T el){
-    head = std::make_shared<DLLEl<T>>(el);
+  DoublyLinkedList(T val) {
+    // Why not have them point to each other?
+    // An invariant that we want is that we never want prev/next to point to
+    // oneself Or more generally we never want following prev/next to lead to
+    // any cycles
+    //
+    // An alternative would to only initialise one of head or tail at this
+    // point?
+    head = std::make_shared<DLLEl<T>>(val);
     tail = head;
-  };
+  }
 
-  std::generator<T>
-  getGenerator() {
-    // DLLEl<T>* ptr = &head;
+  void prepend(T val) {
+    auto el = std::make_shared<DLLEl<T>>(val);
+    if (tail == nullptr && head == nullptr) {
+      head = el;
+      tail = el;
+    } else {
+      auto tmp = head;
+      head = el;
+      head->next = tmp;
+      tmp->prev = head;
+    }
+  }
 
-    // while (ptr != &tail) {
-    //   co_yield std::ref(ptr->val);
+  void append(T val) {
+    auto el = std::make_shared<DLLEl<T>>(val);
+    if (tail == nullptr && head == nullptr) {
+      head = el;
+      tail = el;
+    } else {
+      auto tmp = tail;
+      tail = el;
+      tail->prev = tmp;
+      tmp->next = tail;
+    }
+  }
 
-    //   ptr = ptr->next;
-    // }
-    co_yield head->val;
+  std::generator<T> getGenerator() {
+    std::shared_ptr<DLLEl<T>> ptr = head;
+    while (ptr != nullptr) {
+      co_yield ptr->val;
+      ptr = ptr->next;
+    }
   }
 };
--- languages/cpp/playground/exercises/lru_cache/doubly_linked_list_test.cc
+++ languages/cpp/playground/exercises/lru_cache/doubly_linked_list_test.cc
@@ -7,7 +7,28 @@
 
 TEST(DoublyLinkedList, Basics) {
   auto dll = DoublyLinkedList<int>(3);
-
   auto els = dll.getGenerator() | std::ranges::to<std::vector<int>>();
   ASSERT_EQ(els.size(), 1);
+  ASSERT_EQ(els[0], 3);
+
+  dll.append(4);
+  els = dll.getGenerator() | std::ranges::to<std::vector<int>>();
+  ASSERT_EQ(els.size(), 2);
+  ASSERT_EQ(els[1], 4);
+
+  dll.append(5);
+  els = dll.getGenerator() | std::ranges::to<std::vector<int>>();
+  ASSERT_EQ(els.size(), 3);
+  ASSERT_EQ(els[2], 5);
+
+  dll.prepend(10);
+  els = dll.getGenerator() | std::ranges::to<std::vector<int>>();
+  ASSERT_EQ(els.size(), 4);
+  ASSERT_EQ(els[0], 10);
+
+  dll.prepend(11);
+  els = dll.getGenerator() | std::ranges::to<std::vector<int>>();
+  ASSERT_EQ(els.size(), 5);
+  auto eq = els == std::vector<int>{11, 10, 3, 4, 5};
+  ASSERT_TRUE(eq);
 }
