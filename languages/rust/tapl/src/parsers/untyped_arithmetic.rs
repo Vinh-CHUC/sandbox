cd90ea2621d7fb7ffd74c2dc1e3734c7ecfed314
Vinh Chuc <vinh.chuc@skyral.io>
2025-10-28 09:09:04 +0000

    Lint

--- languages/rust/tapl/src/parsers/untyped_arithmetic.rs
+++ languages/rust/tapl/src/parsers/untyped_arithmetic.rs
@@ -4,20 +4,20 @@ use chumsky::prelude::*;
 pub enum Value {
     True,
     False,
-    Zero
+    Zero,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct If {
     If: Box<Term>,
     Then: Box<Term>,
-    Else: Box<Term>
+    Else: Box<Term>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Term {
-   Value(Value), 
-   If(If),
+    Value(Value),
+    If(If),
 }
 
 impl From<Value> for Term {
@@ -33,7 +33,8 @@ impl From<If> for Term {
 }
 
 fn parse_values<'src>() -> impl Parser<'src, &'src str, Value> {
-    just("true").to(Value::True)
+    just("true")
+        .to(Value::True)
         .or(just("false").to(Value::False))
         .or(just("zero").to(Value::Zero))
 }
@@ -44,20 +45,26 @@ fn parse_term<'src>() -> impl Parser<'src, &'src str, Term> {
         let p_values = parse_values().padded().map(|v| v.into()).boxed();
         let p_inner = p_parens.or(p_values.clone()).boxed();
 
-        let if_p = just("if").ignore_then(p_inner.clone())
+        let if_p = just("if")
+            .ignore_then(p_inner.clone())
             .then(just("then").ignore_then(p_inner.clone()))
             .then(just("else").ignore_then(p_inner))
             .padded()
-            .map(
-                |((vif, vthen), velse)|
-                If {If: Box::new(vif), Then: Box::new(vthen), Else: Box::new(velse)}.into()
-            );
+            .map(|((vif, vthen), velse)| {
+                If {
+                    If: Box::new(vif),
+                    Then: Box::new(vthen),
+                    Else: Box::new(velse),
+                }
+                .into()
+            });
 
         let term = choice((if_p, p_values)).boxed();
         choice((
             term.clone(),
-            term.delimited_by(just('('), just(')')).padded()
-        )).boxed()
+            term.delimited_by(just('('), just(')')).padded(),
+        ))
+        .boxed()
     })
 }
 
@@ -67,7 +74,10 @@ mod tests {
     #[test]
     fn test_values() {
         assert_eq!(parse_values().parse("true").into_result(), Ok(Value::True));
-        assert_eq!(parse_values().parse("false").into_result(), Ok(Value::False));
+        assert_eq!(
+            parse_values().parse("false").into_result(),
+            Ok(Value::False)
+        );
         assert_eq!(parse_values().parse("zero").into_result(), Ok(Value::Zero));
         assert!(parse_values().parse("True").has_errors());
         assert!(parse_values().parse("FAlse").has_errors());
@@ -75,25 +85,41 @@ mod tests {
 
     #[test]
     fn test_term() {
-        assert!(!parse_term().parse("true").has_errors());
-        assert!(!parse_term().parse("if true then false else zero").has_errors());
-        assert!(!parse_term().parse("if (if true then false else zero) then false else zero").has_errors());
-        assert!(!parse_term().parse("(if (if true then false else zero) then false else zero)").has_errors());
-        assert!(!parse_term().parse("  (if (   if true then false else zero) then false else zero)").has_errors());
-        assert!(!parse_term().parse("(true)").has_errors());
-        //
-        // Require brackets around any non value inner expression for clarity
-        assert!(parse_term().parse("if if true then false else zero then false else zero").has_errors());
+        let valid_terms = [
+            "true",
+            "true   ",
+            "if true then false else zero",
+            "if true then   false else zero",
+            "if (if true then false else zero) then false else zero",
+            "if (   if true then false else zero) then false else zero",
+            "(if (if true then false else zero) then false else zero)",
+            "  (if (   if true then false else zero) then false else zero)",
+            "(true)",
+            "if (true) then (false) else (zero)"
+        ];
 
-        let parse = parse_term().parse("if (true) then (false) else (zero)").into_result();
-        assert_eq!(parse,
-            Ok(
-                If {
-                    If: Box::new(Term::Value(Value::True)),
-                    Then: Box::new(Term::Value(Value::False)),
-                    Else: Box::new(Term::Value(Value::Zero))
-                }.into()
-            )
+        for term_str in valid_terms {
+            assert!(!parse_term().parse(term_str).has_errors());
+        }
+
+        // Require brackets around any non value inner expression for clarity
+        assert!(
+            parse_term()
+                .parse("if if true then false else zero then false else zero")
+                .has_errors()
+        );
+
+        let parse = parse_term()
+            .parse("if (true) then (false) else (zero)")
+            .into_result();
+        assert_eq!(
+            parse,
+            Ok(If {
+                If: Box::new(Term::Value(Value::True)),
+                Then: Box::new(Term::Value(Value::False)),
+                Else: Box::new(Term::Value(Value::Zero))
+            }
+            .into())
         );
     }
 }
