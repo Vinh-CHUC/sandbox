4e758b1415cae7e5b0ec87804c113bb249029759
Vinh Chuc <vinh.chuc@skyral.io>
2025-10-29 22:50:24 +0000

    Untyped arithmetic parser done

--- languages/rust/tapl/src/parsers/untyped_arithmetic.rs
+++ languages/rust/tapl/src/parsers/untyped_arithmetic.rs
@@ -9,15 +9,27 @@ pub enum Value {
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct If {
-    If: Box<Term>,
-    Then: Box<Term>,
-    Else: Box<Term>,
+    r#if: Box<Term>,
+    then: Box<Term>,
+    r#else: Box<Term>,
 }
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Succ(Box<Term>);
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Pred(Box<Term>);
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct IsZero(Box<Term>);
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Term {
     Value(Value),
     If(If),
+    Succ(Succ),
+    Pred(Pred),
+    IsZero(IsZero),
 }
 
 impl From<Value> for Term {
@@ -26,9 +38,57 @@ impl From<Value> for Term {
     }
 }
 
+impl From<Value> for Box<Term> {
+    fn from(v: Value) -> Self {
+        Box::new(v.into())
+    }
+}
+
 impl From<If> for Term {
     fn from(r#if: If) -> Self {
         Term::If(r#if)
+    }
+}
+
+impl From<If> for Box<Term> {
+    fn from(r#if: If) -> Self {
+        Box::new(r#if.into())
+    }
+}
+
+impl From<Succ> for Term {
+    fn from(succ: Succ) -> Self {
+        Term::Succ(succ)
+    }
+}
+
+impl From<Succ> for Box<Term> {
+    fn from(succ: Succ) -> Self {
+        Box::new(succ.into())
+    }
+}
+
+impl From<Pred> for Term {
+    fn from(pred: Pred) -> Self {
+        Term::Pred(pred)
+    }
+}
+
+impl From<Pred> for Box<Term> {
+    fn from(pred: Pred) -> Self {
+        Box::new(pred.into())
+    }
+}
+
+impl From<IsZero> for Term {
+    fn from(is_zero: IsZero) -> Self {
+        Term::IsZero(is_zero)
+    }
+}
+
+impl From<IsZero> for Box<Term> {
+    fn from(is_zero: IsZero) -> Self {
+        Box::new(is_zero.into())
     }
 }
 
@@ -39,32 +99,42 @@ fn parse_values<'src>() -> impl Parser<'src, &'src str, Value> {
         .or(just("zero").to(Value::Zero))
 }
 
+fn unary_fn_parse<'src, T: From<Term>>(
+    name: &'src str,
+    inner_p: impl Parser<'src, &'src str, Term>,
+) -> impl Parser<'src, &'src str, T> {
+    just(name).ignore_then(inner_p).map(T::from)
+}
+
 fn parse_term<'src>() -> impl Parser<'src, &'src str, Term> {
     recursive(|p| {
-        let p_parens = p.padded().delimited_by(just('('), just(')')).padded();
+        let p_parens = p.clone().delimited_by(just('('), just(')')).padded();
         let p_values = parse_values().padded().map(|v| v.into()).boxed();
         let p_inner = p_parens.or(p_values.clone()).boxed();
 
         let if_p = just("if")
             .ignore_then(p_inner.clone())
             .then(just("then").ignore_then(p_inner.clone()))
-            .then(just("else").ignore_then(p_inner))
+            .then(just("else").ignore_then(p_inner.clone()))
             .padded()
             .map(|((vif, vthen), velse)| {
                 If {
-                    If: Box::new(vif),
-                    Then: Box::new(vthen),
-                    Else: Box::new(velse),
+                    r#if: Box::new(vif),
+                    then: Box::new(vthen),
+                    r#else: Box::new(velse),
                 }
                 .into()
             });
 
-        let term = choice((if_p, p_values)).boxed();
-        choice((
-            term.clone(),
-            term.delimited_by(just('('), just(')')).padded(),
+        let term = choice((
+            if_p,
+            p_values,
+            unary_fn_parse("succ", p_inner.clone()),
+            unary_fn_parse("pred", p_inner.clone()),
+            unary_fn_parse("iszero", p_inner),
         ))
-        .boxed()
+        .boxed();
+        choice((term.clone(), p.delimited_by(just('('), just(')')).padded())).boxed()
     })
 }
 
@@ -95,7 +165,11 @@ mod tests {
             "(if (if true then false else zero) then false else zero)",
             "  (if (   if true then false else zero) then false else zero)",
             "(true)",
-            "if (true) then (false) else (zero)"
+            "((true))",
+            "if (true) then (false) else (zero)",
+            "succ( if (   if true then false else zero) then false else zero)",
+            "succ true",
+            "succ (succ zero)",
         ];
 
         for term_str in valid_terms {
@@ -115,9 +189,9 @@ mod tests {
         assert_eq!(
             parse,
             Ok(If {
-                If: Box::new(Term::Value(Value::True)),
-                Then: Box::new(Term::Value(Value::False)),
-                Else: Box::new(Term::Value(Value::Zero))
+                r#if: Value::True.into(),
+                then: Value::False.into(),
+                r#else: Value::Zero.into()
             }
             .into())
         );
