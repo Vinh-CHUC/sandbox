659ebaf52b2ff4fe6d11b8ed9500d68de3a72d3f
Vinh Chuc <vinh.chuc@skyral.io>
2025-11-02 09:54:08 +0000

    Fix Sfinae bug

--- languages/cpp/playground/cpp_features/sfinae/sfinae.h
+++ languages/cpp/playground/cpp_features/sfinae/sfinae.h
@@ -1,5 +1,11 @@
 #include<type_traits>
 
+////////////
+////////////
+// Basics //
+////////////
+////////////
+
 ////////////////////////
 // Integral constants //
 ////////////////////////
@@ -28,14 +34,11 @@ template<class T> struct is_reference<T&> : std::integral_constant<int, 1> {};
 template<class T> struct is_reference<T&&> : std::integral_constant<int, 2> {};
 
 template<class T>
-inline constexpr bool is_reference_v = is_reference<T>::value; 
+inline constexpr int is_reference_v = is_reference<T>::value; 
 
 static_assert(is_reference_v<int> == 0);
 static_assert(is_reference_v<int&> == 1);
-
-// Reference collapsing
-// This matches T& with T = int&&
-static_assert(is_reference_v<int&&> == 1);
+static_assert(is_reference_v<int&&> == 2);
 
 // !! Note that this is quite different from normal overload resolution
 // One one case we have different value categories -> choose which overload
@@ -51,16 +54,38 @@ template<class T> struct remove_reference<T&&> : std::integral_constant<int, 2> 
 
 template<class T> using remove_reference_t = typename remove_reference<T>::type;
 
-template<class T> inline constexpr bool remove_reference_v = remove_reference<T>::value; 
-
-static_assert(remove_reference<int>::value == 0);
-static_assert(remove_reference<int&>::value == 1);
-static_assert(remove_reference<int&&>::value == 2);
+template<class T> inline constexpr int remove_reference_v = remove_reference<T>::value; 
 
 static_assert(remove_reference_v<int> == 0);
 static_assert(remove_reference_v<int&> == 1);
-// static_assert(remove_reference_v<int&&> == 2);
+static_assert(remove_reference_v<int&&> == 2);
 
 static_assert(std::is_same_v<remove_reference_t<int>, int>);
 static_assert(std::is_same_v<remove_reference_t<int&>, int>);
 static_assert(std::is_same_v<remove_reference_t<int&&>, int>);
+
+////////////
+// void_t //
+////////////
+template<class...> using void_t = void;
+
+template<class T, typename> struct MyTemplate : std::integral_constant<int, 0> {using type = T; };
+
+// In the template argument list, we want to introduce a type expression such that
+// "it doesn't compile" <==> the specialisation is discarded
+//
+// "it compiles" <==> the specialisation is selected, hence the type expression has to "compile"
+// and also evaluate to the same expression as per the call site
+//
+// So we use this void_t as a trick/default
+template<class T> struct MyTemplate<T, void_t<T&>> : std::integral_constant<int, 1> {using type = T; };
+
+static_assert(MyTemplate<int, void>::value == 1);
+static_assert(MyTemplate<void, void>::value == 0);
+
+// Using a default type = void to make this more ergonomic
+template<class T, typename U = void> struct MyTemplate2 : std::integral_constant<int, 0> {using type = T; };
+template<class T> struct MyTemplate2<T, void_t<T&>> : std::integral_constant<int, 1> {using type = T; };
+
+static_assert(MyTemplate2<int>::value == 1);
+static_assert(MyTemplate2<void>::value == 0);
