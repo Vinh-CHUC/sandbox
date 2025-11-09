2d84a26782b0da7cbb08b83d79eb781b183151e7
Vinh Chuc <vinh.chuc@skyral.io>
2025-11-09 11:43:01 +0000

    More Lean4 classical reasoning

--- languages/lean4/playground/Playground/Exercises/LogicAndProof/_5_Classical_Reasoning.lean
+++ languages/lean4/playground/Playground/Exercises/LogicAndProof/_5_Classical_Reasoning.lean
@@ -44,3 +44,105 @@ section
       intro (h2: ¬ A)
       exact h2 h3
 end 
+
+section
+  open Classical
+  variable (A B : Prop)
+
+  example (h : ¬ B → ¬ A) : A → B := by
+    intro (h1 : A) 
+    show B
+    apply byContradiction
+    intro (h2 : ¬ B)
+    have h3 : ¬ A := h h2
+    show  False
+    exact h3 h1
+
+  example (h : ¬ (A ∧ ¬ B)): A → B := by
+    intro a
+    show B
+    apply byContradiction
+    intro nb
+    have : A ∧ ¬ B := And.intro a nb
+    show False
+    apply h
+    assumption
+
+  example : (A → B) ∨ (B → A) :=
+  Or.elim (em B)
+    (fun h : B ↦ 
+      have : A → B :=
+        fun _ : A ↦ show B from h
+      show (A → B) ∨ (B → A) from Or.inl this)
+    (fun h: ¬ B ↦
+      have : B → A :=
+        fun _: B ↦
+          have : False := h ‹ B ›
+          show A from False.elim this
+      show (A → B) ∨ (B → A) from Or.inr this)
+
+  /- Tactical mode -/
+  example : (A → B) ∨ (B → A) := by
+  apply Or.elim (em B)
+  . intro b
+    have : A → B := by
+      intro
+      assumption
+    apply Or.inl
+    assumption
+  . intro nb
+    have : B → A := by
+      intro b
+      apply False.elim
+      exact nb b
+    apply Or.inr
+    assumption
+end 
+
+/- ---------------------------- -/
+/- 5.3 The contradiction tactic -/
+/- ---------------------------- -/
+section
+  open Classical
+  variable (A: Prop)
+
+  /- Law of excluded middle -/
+  example: A ∨ ¬ A := by
+    apply byContradiction
+    intro (h1 : ¬ (A ∨ ¬ A))
+    have h2 : ¬ A := by
+      intro (h3: A)
+      have h4: A ∨ ¬ A := Or.inl h3
+      show False
+      exact h1 h4
+    have h5: A ∨ ¬ A := Or.inr h2
+    show False
+    exact h1 h5
+
+
+  /- Same but with contradiction tactic -/
+  example: A ∨ ¬ A := by
+    apply byContradiction
+    intro (h1 : ¬ (A ∨ ¬ A))
+    have h2 : ¬ A := by
+      intro (h3: A)
+      have h4: A ∨ ¬ A := Or.inl h3
+      /- Will automatically search for h1 h4? -/
+      /- e.g. something of the form A and ¬ A -/
+      contradiction
+    have h5: A ∨ ¬ A := Or.inr h2
+    contradiction
+
+  /- Even more concise -/
+  example: A ∨ ¬ A := by
+    apply byContradiction
+    intro
+    have : ¬ A := by
+      intro
+      have : A ∨ ¬ A := Or.inl ‹ A ›
+      /- Will automatically search for h1 h4? -/
+      /- e.g. something of the form A and ¬ A -/
+      contradiction
+    have : A ∨ ¬ A := Or.inr  ‹ ¬ A ›
+    contradiction
+end
