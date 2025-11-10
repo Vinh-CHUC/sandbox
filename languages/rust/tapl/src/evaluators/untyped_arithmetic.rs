ce7116ddeba8e46736a94e49bc0714dd426b86e0
Vinh Chuc <vinh.chuc@gmail.com>
2025-11-10 22:51:20 +0000

    More Rust parsing

--- languages/rust/tapl/src/evaluators/untyped_arithmetic.rs
+++ languages/rust/tapl/src/evaluators/untyped_arithmetic.rs
@@ -1,7 +1,12 @@
 use crate::parsers::untyped_arithmetic::{parse_term, Term};
 
+use chumsky::prelude::*;
+
 pub fn eval(t: Term) -> Term {
     match t {
+        Term::Zero => t,
+        Term::True => t,
+        Term::False => t,
         _ => t
     }
 }
@@ -11,5 +16,11 @@ mod tests {
 
     #[test]
     fn test_values() {
+        let t = parse_term().parse("true").into_result().unwrap();
+        assert_eq!(t, eval(t.clone()));
+        let t = parse_term().parse("false").into_result().unwrap();
+        assert_eq!(t, eval(t.clone()));
+        let t = parse_term().parse("(zero)").into_result().unwrap();
+        assert_eq!(t, eval(t.clone()));
     }
 }
