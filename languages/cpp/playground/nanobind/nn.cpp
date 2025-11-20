1574667017a3d4e05f18d81ff867cbebe955a10e
Vinh Chuc <vinh.chuc@skyral.io>
2025-11-20 20:47:36 +0000

    BKP

--- languages/cpp/playground/nanobind/nn.cpp
+++ languages/cpp/playground/nanobind/nn.cpp
@@ -1,7 +1,14 @@
 #include <nanobind/nanobind.h>
 
-int add(int a, int b) { return a + b; }
+namespace nb = nanobind;
 
-NB_MODULE(my_ext, m) {
-    m.def("add", &add);
+using namespace nb::literals;
+
+NB_MODULE(nanobind_example_ext, m) {
+    m.doc() = "This is a \"hello world\" example with nanobind";
+    m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
+
+    nb::module_ sm = m.def_submodule("sub_ext", "A submodule of 'nanobind_example_ext'");
+
+    sm.def("sub", [](int a, int b) { return a - b; }, "a"_a, "b"_a);
 }
