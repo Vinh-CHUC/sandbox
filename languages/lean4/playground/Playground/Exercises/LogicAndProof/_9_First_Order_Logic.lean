175630b4c0d62948a141c626ff39d0af8d9274d2
Vinh Chuc <vinh.chuc@gmail.com>
2026-02-28 22:41:25 +0000

    Lean4 backup

--- languages/lean4/playground/Playground/Exercises/LogicAndProof/_9_First_Order_Logic.lean
+++ languages/lean4/playground/Playground/Exercises/LogicAndProof/_9_First_Order_Logic.lean
@@ -1,3 +1,5 @@
+import Mathlib.Data.Nat.Prime.Basic
+
 -- 9.1 Functions Predicates and Relations
 -- Axiom programming equivalent: declared but not defined?
 -- Deeper:
@@ -30,6 +32,41 @@ variable (x y: U)
 #check Type 1
 
 #check Nat
+
+--
+-- Example of Propositions ranging on Nat
+--
+
+namespace hidden
 -- 1 for precedence
-notation: 1 "ℕ" => Nat
+notation:1 "ℕ" => Nat
 #check ℕ
+
+axiom mul : ℕ → (ℕ → (ℕ))
+axiom add : ℕ → (ℕ → (ℕ))
+axiom square : Nat → Nat
+axiom even : ℕ → Prop
+axiom odd : ℕ → Prop
+axiom prime : ℕ → Prop
+axiom divides : ℕ → (ℕ → Prop)
+axiom lt : ℕ → (ℕ → Prop)
+axiom zero : ℕ
+axiom one : ℕ
+
+variable (w x y z: ℕ)
+#check mul x y
+
+-- The priority := has to do with typeclass resolution (~overload)
+local infix:65 (priority := high)  " + " => add
+local infix:70 (priority := high)  " * " => mul
+local infix:50 (priority := high)  " < " => lt
+
+#check even (x + y + z) ∧ prime ((x + one) * y * y)
+#check ¬ (square (x + y * z) = w) ∨ x + y < z
+#check x < y ∧ even x ∧ even y → x + one < y
+
+end hidden
+
+--------------------------------------------
+-- 9.2 Functions Predicates and Relations --
+--------------------------------------------
