b922cf7eba0ab9ff4195efe34d8ffe65ed31d898
Vinh Chuc <vinh.chuc@gmail.com>
2026-05-18 07:04:50 +0100

    Lean4 Started Sets in Lean

--- languages/lean4/playground/Playground.lean
+++ languages/lean4/playground/Playground.lean
@@ -5,3 +5,4 @@ import Playground.Exercises.FPInLean._1_GettingToKnowLean
 import Playground.Exercises.LogicAndProof._4_Propositional_Logic
 import Playground.Exercises.LogicAndProof._5_Classical_Reasoning
 import Playground.Exercises.LogicAndProof._9_First_Order_Logic
+import Playground.Exercises.LogicAndProof._12_Sets
