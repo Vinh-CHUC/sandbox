6c2af2c6626ce63291d7b0813c1d8b26d33313ac
Vinh Chuc <vinh.chuc@gmail.com>
2026-05-24 19:38:40 +0100

    Lean4 more sets

--- languages/lean4/playground/Playground/Exercises/LogicAndProof/_12_Sets.lean
+++ languages/lean4/playground/Playground/Exercises/LogicAndProof/_12_Sets.lean
@@ -1,5 +1,6 @@
 import Mathlib.Data.Set.Basic
 import Mathlib.Order.BooleanAlgebra.Set
+import Mathlib.Order.SetNotation
 open Set
 
 -----------------
@@ -124,3 +125,101 @@ theorem inter_union_subset {x} :
     show x ∈ (A ∩ B) ∪ (A ∩ C)
     apply Or.inr
     assumption
+
+theorem inter_union_inter_subset {x} :
+  (x ∈ (A ∩ B) ∪ (A ∩ C)) → (x ∈ A ∩ (B ∪ C)) := by
+  intro (hx : x ∈ (A ∩ B) ∪ (A ∩ C))
+  cases hx with
+  | inl h =>
+    show x ∈ A ∩ (B ∪ C)
+    apply And.intro
+    . show x ∈ A
+      exact h.left
+    . show x ∈ (B ∪ C)
+      exact Or.inl h.right
+  | inr h =>
+    show x ∈ A ∩ (B ∪ C)
+    apply And.intro
+    . show x ∈ A
+      exact h.left
+    . show x ∈ (B ∪ C)
+      exact Or.inr h.right
+
+example : A ∩ (B ∪ C) = (A ∩ B) ∪ (A ∩ C) := by
+  ext x
+  constructor
+  . exact inter_union_subset A B C
+  . exact inter_union_inter_subset A B C
+
+example : (A ∩ Bᶜ) ∪ B = A ∪ B :=
+calc
+  (A ∩ Bᶜ) ∪ B = (A ∪ B) ∩ (Bᶜ ∪ B) := by rw [inter_union_distrib_right]
+  _ = (A ∪ B) ∩ univ := by rw [compl_union_self]
+  _ = (A ∪ B) := by rw [inter_univ]
+
+---------------------------
+-- 12.3 Indexed Families --
+---------------------------
+
+variable {I U : Type}
+
+
+def myiUnion(A : I → Set U) : Set U := {x | ∃ i: I, x ∈ A i}
+def myiInter(A : I → Set U) : Set U := {x | ∀ i: I, x ∈ A i}
+
+section
+variable (x : U) (A: I → Set U)
+
+-- In this case Lean can "definitionally unfold"
+example (h : x ∈ myiUnion A) : ∃ i, x ∈ A i := h
+example (h : x ∈ myiInter A) : ∀ i, x ∈ A i := h
+end
+
+-- The actual iUnion and iInter
+#check mem_iUnion
+#check mem_iInter
+
+variable {A B : I → Set U}
+
+theorem exists_of_mem_Union {x : U}
+(h : x ∈ ⋃ i, A i):
+∃ i, x ∈ A i := by
+    rw [← mem_iUnion]
+    assumption
+
+theorem mem_Union_of_exists {x : U}
+(h : ∃ i, x ∈ A i):
+x ∈ ⋃ i, A i := by
+    rw [mem_iUnion]
+    assumption
+
+theorem forall_of_mem_Inter {x : U}
+(h : x ∈ ⋂ i, A i):
+∀ i, x ∈ A i := by
+rw [← mem_iInter]
+assumption
+
+theorem mem_Inter_of_forall {x : U}
+(h: ∀ i, x ∈ A i) :
+(x ∈ ⋂ i, A i):= by
+rw [mem_iInter]
+assumption
+
+-- An example
+example : (⋂ i, A i ∩ B i) = (⋂ i, A i) ∩ (⋂ i, B i) := by
+  ext x
+  show x ∈ ⋂ i, A i ∩ B i ↔ x ∈ (⋂ i, A i) ∧ x ∈ ⋂ i, B i
+  rw [mem_iInter, mem_iInter, mem_iInter]
+  show (∀ (i: I), x ∈ A i ∧ x ∈ B i) ↔
+    (∀ (i: I), x ∈ A i) ∧ (∀ (i : I), x ∈ B i)
+  constructor
+  . intro (h : (∀ (i: I), x ∈ A i ∧ x ∈ B i))
+    show (∀ (i: I), x ∈ A i) ∧ (∀ (i : I), x ∈ B i)
+    constructor 
+    . show (∀ (i: I), x ∈ A i)
+      exact fun j ↦ And.left $ h j
+    . show (∀ (i: I), x ∈ B i)
+      exact fun j ↦ And.right $ h j
+  . intro (h: (∀ (i: I), x ∈ A i) ∧ (∀ (i : I), x ∈ B i))
+    show ∀ i, x ∈ A i ∧ x ∈ B i
+    exact fun j ↦ ⟨h.left j, h.right j⟩
