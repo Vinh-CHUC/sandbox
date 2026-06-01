b6db96060269a9cee0cb1c072987007bcd7c26ec
Vinh Chuc <vinh.chuc@skyral.io>
2026-06-01 11:59:00 +0100

    More chumsky

--- languages/rust/tapl/src/chumsky_playground/basics.rs
+++ languages/rust/tapl/src/chumsky_playground/basics.rs
@@ -1,5 +1,7 @@
 //! ## Input trait
 //!
+//! Compared to normal iterator allows backtracking
+//!
 //! ### Fails to compile: Owned string as input with 'src lifetime
 //! ```compile_fail
 //! use chumsky::prelude::*;
@@ -15,16 +17,33 @@
 //! use chumsky::Parser;
 //! let s = String::from("input");
 //! // noop_parser expects &'src str, but we passed a String
-//! let _ = noop_parser().parse(s); 
+//! let _ = noop_parser().parse(s);
 //! ```
 
 use chumsky::prelude::*;
+
+/*
+ *
+ * PRIMITIVES
+ *
+ */
 
 // 'src : lifetime of the input
 // &'src str : the input type
 // (): output type
 pub fn noop_parser<'src>() -> impl Parser<'src, &'src str, ()>{
     end()
+}
+
+// just
+//
+// The output type of the parser is basically Just<T> where T is the input to just(seq: T)
+pub fn just_char_parser<'src>() -> impl Parser<'src, &'src str, char> {
+    just('h')
+}
+
+pub fn just_str_parser<'src>() -> impl Parser<'src, &'src str, &'src str> {
+    just("hello")
 }
 
 #[cfg(test)]
@@ -35,5 +54,14 @@ mod tests {
     fn test_noop_parser(){
         assert_eq!(noop_parser().parse("").into_result(), Ok(()));
         assert!(noop_parser().parse("yo").has_errors());
+    }
+
+    #[test]
+    fn test_just_parsers() {
+        assert_eq!(just_char_parser().parse("h").into_result(), Ok('h'));
+        assert_eq!(just_str_parser().parse("hello").into_result(), Ok("hello"));
+
+        assert!(just_char_parser().parse("x").has_errors());
+        assert!(just_str_parser().parse("world").has_errors());
     }
 }
---
+++ languages/rust/tapl/src/chumsky_playground/state.rs
@@ -0,0 +1,2 @@
+// TODO Maybe
+// Look at how to have stateful parser from the chumsky docs
