6e8dca07d5030f613f5df463e709a475a1dbf61b
Vinh Chuc <vinh.chuc@skyral.io>
2026-06-08 17:37:50 +0100

    Chumsky: let expressions (incomplete)

--- languages/rust/tapl/src/chumsky_tutorial/mod.rs
+++ languages/rust/tapl/src/chumsky_tutorial/mod.rs
@@ -26,13 +26,18 @@ pub enum Expr<'src> {
 }
 
 fn parser<'src>() -> impl Parser<'src, &'src str, Expr<'src>> {
-    recursive(|expr| {
+    let ident = text::ascii::ident().padded();
+
+    let expr = recursive(|expr| {
         let int = text::int(10)
             // unwrap() is kind of save as we know this will parse integers
             .map(|s: &str| Expr::Num(s.parse().unwrap()))
             .padded();
 
-        let atom = int.or(expr.delimited_by(just('('), just(')'))).padded();
+        let atom = int
+            .or(expr.delimited_by(just('('), just(')'))).padded()
+            .or(ident.map(Expr::Var))
+            .padded();
 
         let op = |c| just(c).padded();
 
@@ -74,7 +79,46 @@ fn parser<'src>() -> impl Parser<'src, &'src str, Expr<'src>> {
         );
 
         sum
-    })
+    });
+
+    let decl = recursive(|decl| {
+        let r#let = text::ascii::keyword("let")
+            .ignore_then(ident)
+            .then_ignore(just('='))
+            .then(expr.clone())
+            .then_ignore(just(';'))
+            .then(decl)
+            .map(|((name, rhs), then) | Expr::Let {
+                name,
+                rhs: Box::new(rhs),
+                then: Box::new(then),
+            });
+
+        r#let
+            .or(expr)
+            .padded()
+    });
+
+    decl
+}
+
+fn eval<'src>(expr: &'src Expr<'src>, vars: &mut Vec<(&'src str, f64)>) -> Result<f64, String> {
+    match expr {
+        Expr::Num(x) => Ok(*x),
+        Expr::Neg(a) => Ok(-eval(a, vars)?),
+        Expr::Add(a, b) => Ok(eval(a, vars)? + eval(b, vars)?),
+        Expr::Sub(a, b) => Ok(eval(a, vars)? - eval(b, vars)?),
+        Expr::Mul(a, b) => Ok(eval(a, vars)? * eval(b, vars)?),
+        Expr::Div(a, b) => Ok(eval(a, vars)? / eval(b, vars)?),
+        Expr::Var(name) => {
+            if let Some((_, val)) = vars.iter().rev().find(|(var, _)| var == name){
+                Ok(*val)
+            } else {
+                Err(format!("Cannot find variable `{}` in scope", name))
+            }
+        }
+        _ => todo!(),
+    }
 }
 
 #[cfg(test)]
@@ -82,13 +126,54 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parser(){
+    fn test_parser_arithmetic_expressions(){
         assert!(!parser().parse("(2+3)*3+4*(---5)").has_errors());
+    }
+
+    #[test]
+    fn test_parser_let_expressions(){
+        let s = r#"
+            let yo = 5 + 3;
+            let a = 5 + a;
+            a
+        "#;
+        assert!(!parser().parse(s).has_errors());
+    }
+
+    #[test]
+    fn test_parser_let_expressions_need_a_tail_value(){
+        let s = r#"
+            let yo = 5 + 3;
+        "#;
+        assert!(parser().parse(s).has_errors());
+    }
+
+    #[test]
+    fn test_eval(){
+        let s = r#"
+            let yo = 5 + 3;
+            let a = 5 + a;
+            a
+        "#;
+        let mut vars = vec![];
+        let ast = parser().parse(s).into_result().unwrap();
+        assert_eq!(eval(&ast, &mut vars).unwrap(), 13.0);
     }
 }
 
-
 pub fn tutorial_main() {
     let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
-    println!("{:?}", parser().parse(&src));
+
+    let mut variables = vec![];
+
+    match parser().parse(&src).into_result() {
+        Ok(ast) => {
+            println!("{:?}", ast);
+            match eval(&ast, &mut variables){
+                Ok(output) => println!("{}", output),
+                Err(eval_err) => println!("Evaluation error {}", eval_err),
+            }
+        }
+        Err(parse_errs) => parse_errs.into_iter().for_each(|e| println!("Parse error: {}", e)),
+    }
 }
